4d6c1f68a87e875ccd7e5b1e8a83baec6fd32c5e
Abisola Oladeji <48631601+a23bisola@users.noreply.github.com>
2023-03-31 13:09:38 +0100

    spacing

--- 03-task-manager/final/app.js
+++ 03-task-manager/final/app.js
@@ -18,16 +18,3 @@ app.use('/api/v1/tasks', tasks);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
-
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
